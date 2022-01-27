d8eb37ee861db570c21c92f914ff1f3e35babcee
Doaahesham <54770485+Doaahesham@users.noreply.github.com>
2022-01-27 14:10:14 +0200

    handle index and script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ links = document.getElementById("links");
 user_info = document.getElementById("user-info");
 user = document.getElementById("user");
 logout = document.getElementById("logout");
+productsInfo = document.querySelector(".products");
+
+// console.log(productsInfo);
 
 if (localStorage.getItem("username")) {
   links.remove();
@@ -15,3 +18,62 @@ logout.addEventListener("click", function () {
     window.location = "register.html";
   }, 2000);
 });
+
+let products = [
+  {
+    id: 1,
+    title: "coffe machine",
+    description: "Lorem ipsum dolor sit amet consecteisi dolore!",
+    size: "large",
+    imageUrl: "images/coffeemachine.png",
+  },
+  {
+    id: 2,
+    title: "cattle",
+    description: "Lorem ipsum dolor sit amet consecteisi dolore!",
+    size: "medium",
+    imageUrl: "images/katl.png",
+  },
+  {
+    id: 3,
+    title: "madrb",
+    description: "Lorem ipsum dolor sit amet consecteisi dolore!",
+    size: "large",
+    imageUrl: "images/madrb.png",
+  },
+  {
+    id: 4,
+    title: "botgaz",
+    description: "Lorem ipsum dolor sit amet consecteisi dolore!",
+    size: "medium",
+    imageUrl: "images/botgaz.png",
+  },
+];
+
+// draw
+
+function draw() {
+  let productsUi = products.map((item) => {
+    return `
+    <div class="product-itm">
+    <img
+      src="${item.imageUrl}"
+      alt="img"
+      class="product-itm-img"
+    />
+    <div class="product-itm-desc">
+      <h3>${item.title}</h3>
+      <p>${item.description}</p>
+      <span>size: ${item.size}</span>
+    </div>
+    <div class="product-itm-actions">
+      <button class="Add-to-Cart">Add to Cart</button>
+      <i class="fav far fa-heart"></i>
+    </div>
+  </div>
+  `
+  });
+
+  productsInfo.innerHTML = productsUi;
+}
+draw();
